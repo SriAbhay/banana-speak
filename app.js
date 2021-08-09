b6e9045ffb4b45b6700956d91c304af0c1b2f7c7
SriAbhay <abhaybbk@gmail.com>
2021-08-09 23:30:59 +0530

    update css

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log("txtInput");
-btnTranslate.addEventListener("click", function clickEventHandler()
+console.log("txtInput")
+
+function clickHandler()
 {
     console.log("clicked!")
-})
+};
+btnTranslate.addEventListener("click", function clickEventHandler()
