1b2c471a3753301a908218b7c94adab523b3e3d2
SriAbhay <abhaybbk@gmail.com>
2021-08-10 02:16:41 +0530

    update js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function clickHandler()
-{
-    outputDiv.innerText = "ajajajsjsajj -> " + txtInput.value;
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function clickHandler() {
+    //outputDiv.innerText = "ajajajsjsajj : " + txtInput.value;
+    var inputText = txtInput.nodeValue;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => console.log(json.contents.translated))
+
 };
 
 btnTranslate.addEventListener("click", clickHandler)
