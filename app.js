67fec25df32e86268638060c3d106bc06eac26ca
SriAbhay <abhaybbk@gmail.com>
2021-08-10 02:42:52 +0530

    update js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(input) {
-    return serverURL + "?" + "text=" + Input
+function getTranslationURL(Text) {
+    return serverURL + "?" + "text=" + Text
 }
 
 function clickHandler() {
