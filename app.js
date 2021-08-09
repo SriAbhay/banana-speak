5cccadc7c826722c5e023a6e41841fcd849f1eab
SriAbhay <abhaybbk@gmail.com>
2021-08-10 03:33:42 +0530

    update js

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ function clickHandler() {
     var inputText = txtInput.value;
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
-        .then(json => { 
-                var translatedText = json.contents.translated;
-                outputDiv.innerText = translatedText;
-            })
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
 
 };
 btnTranslate.addEventListener("click", clickHandler)
