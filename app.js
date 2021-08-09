4c966853080731b5c3052fac75fcbf6bfc14e1df
SriAbhay <abhaybbk@gmail.com>
2021-08-10 00:25:20 +0530

    update css

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-outputDiv.innerText = "output"
-
-console.log("txtInput")
-
 function clickHandler()
 {
-    outputDiv.innerText = "ajajajsjsajj  " +txtInput.value:
+    outputDiv.innerText = "ajajajsjsajj  " +txtInput.value;
 };
+
 btnTranslate.addEventListener("click", clickHandler)
