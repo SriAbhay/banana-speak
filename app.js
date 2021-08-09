1a83ce29474fefe990debbe51a221c9646085fa6
SriAbhay <abhaybbk@gmail.com>
2021-08-10 00:14:12 +0530

    update css and html

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+outputDiv.innerText = "Abhay Srivastava"
+
 console.log("txtInput")
 
 function clickHandler()
